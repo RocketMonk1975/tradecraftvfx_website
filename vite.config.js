158bcb3829436932466ab2c0f9baf8c79a541aa0
Anthony Vu <anthony.vu@tradecraftvfx.vn>
2025-03-10 00:29:32 +0700

    Fix GitHub Pages deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,1 @@
-﻿import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/tradecraftvfx_website/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+﻿
