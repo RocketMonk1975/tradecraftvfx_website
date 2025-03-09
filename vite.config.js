5b3912a527629faf2885aa9b1f1f9a874b9a8141
Anthony Vu <anthony.vu@tradecraftvfx.vn>
2025-03-09 16:18:29 +0700

    Add GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
+  base: '/tradecraftvfx_website/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
-  }
-});
+    },
+  },
+})
