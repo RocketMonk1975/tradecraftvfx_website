d53537099fd9e48bb391a5f1d746adcf5e3389ac
Anthony Vu <anthony.vu@tradecraftvfx.vn>
2025-03-09 22:09:00 +0700

    Fix main.js - only include routes for existing page components

--- src/main.js
+++ src/main.js
@@ -4,24 +4,18 @@ import App from './App.vue'
 import './assets/styles/variables.css'
 import './assets/styles/base.css'
 
-// Import routes
-import HomePage from './pages/HomePage.vue'
+// Import routesimport HomePage from './pages/HomePage.vue'
 import AboutPage from './pages/AboutPage.vue'
 import ServicesPage from './pages/ServicesPage.vue'
 import WorkPage from './pages/WorkPage.vue'
-import ProjectPage from './pages/ProjectPage.vue'
 import NewsPage from './pages/NewsPage.vue'
-import ContactPage from './pages/ContactPage.vue'
 
 // Create router
-const routes = [
-  { path: '/', component: HomePage },
+const routes = [  { path: '/', component: HomePage },
   { path: '/about', component: AboutPage },
   { path: '/services', component: ServicesPage },
   { path: '/work', component: WorkPage },
-  { path: '/work/:id', component: ProjectPage },
   { path: '/news', component: NewsPage },
-  { path: '/contact', component: ContactPage }
 ]
 
 const router = createRouter({
